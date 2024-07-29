7aec9f1aa194610a1b427071933bb43cf530b247
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2024-07-29 00:37:10 -0300

    styled components configurado

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -6,5 +6,13 @@ const globalStyles = createGlobalStyle`
         margin:0;
         padding: 0;
         box-sizing: border-box;
-    }   
+        outline: none;
+    } 
+    
+    button{
+        cursor: pointer;
+    }
+
 `;
+
+export default globalStyles;
