30bb0f0014e665e4b7ef4a2ec87761d5f23162ec
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2024-07-29 23:24:07 -0300

    criando o html da página de login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,30 @@
+import Logo from '../../assets/logo.svg';
+import { Button, Container, Form, InputContainer, LeftContainer, Link, RigthContainer, Titile } from './style';
+
 export default function Login() {
 	return (
-		<div>
-			<h1>Login</h1>
-		</div>
+		<Container>
+			<LeftContainer>
+				<img src={Logo} alt="logo-devBurguer" />
+			</LeftContainer>
+			<RigthContainer>
+				<Titile>
+					Olá, seja bem vindo ao <span>Dev Burguer!</span>Acesse com seu <span>Login e senha.</span>
+				</Titile>
+				<Form>
+					<InputContainer>
+						<label>Email</label>
+						<input type="email" />
+					</InputContainer>
+					<InputContainer>
+						<label>Senha</label>
+						<input type="password" />
+					</InputContainer>
+					<Link>Esqueci minha senha.</Link>
+					<Button>Entrar</Button>
+				</Form>
+				<Link>Não possui conta? Clique aqui.</Link>
+			</RigthContainer>
+		</Container>
 	);
 }
